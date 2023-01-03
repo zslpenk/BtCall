0ba839e68d686314a3362cb136dde4be4fb89819
zsl <674477021@qq.com>
2023-01-03 10:43:16 +0800

    Test use Text

--- BtCall/app/src/main/java/com/autolink/btcall/MainActivity.kt
+++ BtCall/app/src/main/java/com/autolink/btcall/MainActivity.kt
@@ -3,13 +3,16 @@ package com.autolink.btcall
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -33,13 +36,13 @@ class MainActivity : ComponentActivity() {
                     init(mainViewModel)
                 }
 
-//                Column(Modifier.padding(20.dp)) {
-//                    Text(text = "Text 1 !", color = MaterialTheme.colorScheme.error)
-//                    Text(text = "Text 2 !", color = MaterialTheme.colorScheme.secondary)
-//                    Text(text = "Text 3 !", color = MaterialTheme.colorScheme.primary)
-//                    Text(text = "Text 4 !", color = MaterialTheme.colorScheme.tertiary)
-//                    Text(text = "Text 5 !", color = MaterialTheme.colorScheme.inversePrimary)
-//                }
+                Column(Modifier.padding(20.dp)) {
+                    Text(text = "Text 1 !", color = MaterialTheme.colorScheme.error)
+                    Text(text = "Text 2 !", color = MaterialTheme.colorScheme.secondary)
+                    Text(text = "Text 3 !", color = MaterialTheme.colorScheme.primary)
+                    Text(text = "Text 4 !", color = MaterialTheme.colorScheme.tertiary)
+                    Text(text = "Text 5 !", color = MaterialTheme.colorScheme.inversePrimary)
+                }
             }
         }
     }
